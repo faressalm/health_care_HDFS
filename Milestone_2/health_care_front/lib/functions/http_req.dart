cbcc6dd23d8b8edb0a5d474b9a9d9f5c56d6e839
faressalm <faressalm44@hotmail.com>
2022-04-03 13:13:37 +0200

    get with date range

--- Milestone_2/health_care_front/lib/functions/http_req.dart
+++ Milestone_2/health_care_front/lib/functions/http_req.dart
@@ -1,10 +1,15 @@
-  import 'dart:convert';
+  import 'dart:_http';
+import 'dart:convert';
 
 import 'package:health_care_front/models/heallth_service.dart';
 import 'package:http/http.dart' as http;
 
-Future<List<HealthService>> getheallthServicesList() async {
-    var data = await http.get(Uri.parse('http://localhost:8080/getallservices'));
+Future<List<HealthService>> getheallthServicesList(DateTime start,DateTime end) async {
+  final uri = Uri.parse('http://localhost:8080/getallservices').replace(queryParameters: {
+      'start': start.millisecondsSinceEpoch/1000,
+      'end': end.millisecondsSinceEpoch/1000,
+    });
+    var data = await http.get(uri);
     var jsonData = json.decode(data.body);
     List<HealthService> heallthServicesList = jsonData.map((data)=>HealthService.fromJson(data)).toList();
     // List<HealthService> heallthServicesLis =[];
